72de3b3225f403a98499e8aea3da26c85fcf433e
Priyankanegi28 <negipriyanka949@gmail.com>
2024-12-26 16:42:58 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+if(document.title =="Login Page"){
+    const loginForm = document.getElementById("loginForm");
+    const errorMessage = document.getElementById("errorMessage");
+
+    loginForm.addEventListener
+}
